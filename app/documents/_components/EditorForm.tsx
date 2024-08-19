564afc94f1ea172013e6db5df70cd1ea2f2325d0
Amr Khaled <lewolfje@gmail.com>
2024-08-19 15:30:40 +0300

    refactor: Simplify EditorForm component and remove unused code

--- app/documents/_components/EditorForm.tsx
+++ app/documents/_components/EditorForm.tsx
@@ -6,68 +6,77 @@ import { Button } from "@/components/ui/button";
 import {
   Form,
   FormControl,
-  FormDescription,
   FormField,
   FormItem,
   FormLabel,
   FormMessage,
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
-import Editor from "@/components/Editor";
+import { useRouter } from "next/navigation";
 
 const editorFormSchema = z.object({
   title: z.string().min(1, "Title must be at least 1 character long"),
-  content: z.string().min(1, "Content must be at least 1 character long"),
 });
 
-type EditorFormValues = z.infer<typeof editorFormSchema>;
+export type EditorFormValues = z.infer<typeof editorFormSchema>;
 
-type Props = {
-  document: string;
-};
+type Props = {};
 
-const EditorForm = ({ document }: Props) => {
+const EditorForm = (props: Props) => {
+  const router = useRouter();
   const form = useForm<EditorFormValues>({
     resolver: zodResolver(editorFormSchema),
-    defaultValues: { title: "", content: "" },
+    defaultValues: { title: "" },
   });
 
   const handleSubmit = async (values: EditorFormValues) => {
-    console.log(values);
+    router.push(`/documents/${values.title}`);
   };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-4'>
-        <FormField
-          control={form.control}
-          name='title'
-          render={({ field }) => (
-            <FormItem>
-              <FormLabel>Document Title</FormLabel>
-              <FormControl>
-                <Input {...field} placeholder='Document Title' />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <FormField
-          control={form.control}
-          name='content'
-          render={({ field }) => (
-            <FormItem>
-              <FormControl>
-                <Editor content={field.value} onChange={field.onChange} document={document} />
-              </FormControl>
-              <FormMessage />
-            </FormItem>
-          )}
-        />
-        <Button type='submit'>Save</Button>
+        <div className='flex justify-center w-full gap-4'>
+          <FormField
+            control={form.control}
+            name='title'
+            render={({ field }) => (
+              <FormItem>
+                <FormLabel>Document Title</FormLabel>
+                <FormControl>
+                  <Input {...field} placeholder='Enter document title...' />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+        </div>
+
+        <div className='flex justify-center'>
+          <Button type='submit'>Go to Document</Button>
+        </div>
       </form>
     </Form>
   );
 };
 
 export default EditorForm;
+
+/**
+ * <FormField
+          control={form.control}
+          name='content'
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Editor
+                  content={field.value}
+                  onChange={field.onChange}
+                  document={document}
+                />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+ */
