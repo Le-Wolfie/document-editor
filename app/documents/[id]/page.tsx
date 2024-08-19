fbd8a3c6a65e8df1aad550b2f18155bc7f313ede
Amr Khaled <lewolfje@gmail.com>
2024-08-19 15:31:20 +0300

    refactor: Update page component to include Editor and Back button

--- app/documents/[id]/page.tsx
+++ app/documents/[id]/page.tsx
@@ -1,11 +1,25 @@
 import React from "react";
 import EditorForm from "../_components/EditorForm";
+import Editor from "@/components/Editor";
+import { ArrowLeft, SendToBackIcon } from "lucide-react";
+import Link from "next/link";
 
 const page = ({ params: { id } }: { params: { id: string } }) => {
   return (
-    <div className='flex flex-col w-full h-full items-center justify-center'>
-      <EditorForm document={id} />
-    </div>
+    <>
+      <div className='absolute top-6 left-8'>
+        <Link
+          href='/documents'
+          className='flex items-center space-x-2 hover:underline cursor-pointer'
+        >
+          <ArrowLeft className='h-4 w-4' />
+          <span>Back</span>
+        </Link>
+      </div>
+      <div className='flex flex-col w-full h-full items-center justify-center'>
+        <Editor document={id} />
+      </div>
+    </>
   );
 };
 
