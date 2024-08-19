7a45696d8255ac4cba6566a5c4948ec42f291dc8
Amr Khaled <lewolfje@gmail.com>
2024-08-19 13:02:52 +0300

    refactor: Update page component to use dynamic document ID

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -1,12 +1,9 @@
 import React from "react";
-import EditorForm from "./_components/EditorForm";
 
-const page = () => {
-  return (
-    <div className='flex flex-col w-full h-full items-center justify-center'>
-      <EditorForm />
-    </div>
-  );
+type Props = {};
+
+const page = (props: Props) => {
+  return <div>Main Documents</div>;
 };
 
 export default page;
