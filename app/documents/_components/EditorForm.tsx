1e988ef628fe0ad13aaf742c26c921fb422cda0b
Amr Khaled <lewolfje@gmail.com>
2024-08-20 18:27:50 +0300

    refactor: Improve form submission handling and add document creation success notification

--- app/documents/_components/EditorForm.tsx
+++ app/documents/_components/EditorForm.tsx
@@ -13,6 +13,9 @@ import {
 } from "@/components/ui/form";
 import { Input } from "@/components/ui/input";
 import { useRouter } from "next/navigation";
+import { createDocumentAction } from "../_actions/document.actions";
+import { toast } from "sonner";
+import { Loader2 } from "lucide-react";
 
 const editorFormSchema = z.object({
   title: z.string().min(1, "Title must be at least 1 character long"),
@@ -20,9 +23,7 @@ const editorFormSchema = z.object({
 
 export type EditorFormValues = z.infer<typeof editorFormSchema>;
 
-type Props = {};
-
-const EditorForm = (props: Props) => {
+const EditorForm = () => {
   const router = useRouter();
   const form = useForm<EditorFormValues>({
     resolver: zodResolver(editorFormSchema),
@@ -30,19 +31,29 @@ const EditorForm = (props: Props) => {
   });
 
   const handleSubmit = async (values: EditorFormValues) => {
+    const response = await createDocumentAction(values);
+    if (!response.success) {
+      toast.error(response.error);
+      return;
+    }
+
+    toast.success("Document created successfully");
+
     router.push(`/documents/${values.title}`);
   };
 
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(handleSubmit)} className='space-y-4'>
-        <div className='flex justify-center w-full gap-4'>
+        <div className='flex flex-col justify-center items-center w-full gap-4'>
           <FormField
             control={form.control}
             name='title'
             render={({ field }) => (
               <FormItem>
-                <FormLabel>Document Title</FormLabel>
+                <FormLabel className='flex justify-center my-2'>
+                  Document Title
+                </FormLabel>
                 <FormControl>
                   <Input {...field} placeholder='Enter document title...' />
                 </FormControl>
@@ -53,7 +64,13 @@ const EditorForm = (props: Props) => {
         </div>
 
         <div className='flex justify-center'>
-          <Button type='submit'>Go to Document</Button>
+          <Button type='submit' disabled={form.formState.isSubmitting}>
+            {form.formState.isSubmitting ? (
+              <Loader2 className='animate-spin' />
+            ) : (
+              "Create Document"
+            )}
+          </Button>
         </div>
       </form>
     </Form>
