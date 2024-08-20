508b197c1b4e7622059e97f70a73db2aec6fd2c1
Amr Khaled <lewolfje@gmail.com>
2024-08-20 23:32:42 +0300

    feat: Add permanent redirect from root to documents page

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  redirects: async () => {
+    return [
+      {
+        source: "/",
+        destination: "/documents",
+        permanent: true,
+      },
+    ];
+  },
+};
 
 export default nextConfig;
