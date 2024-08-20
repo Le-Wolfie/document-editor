8e2ad70d7bac9c495827d9af7261b31af7659128
Amr Khaled <lewolfje@gmail.com>
2024-08-20 18:26:09 +0300

    refactor: Update page component to include document title and list of documents

--- app/documents/page.tsx
+++ app/documents/page.tsx
@@ -1,13 +1,37 @@
 import React from "react";
 import EditorForm from "./_components/EditorForm";
+import { createClerkSupabaseClient } from "@/utils/supabase/client";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
 type Props = {};
 
-const page = (props: Props) => {
+const page = async (props: Props) => {
+  const supabase = createClerkSupabaseClient();
+  const { data } = await supabase.from("documents").select("title");
+  console.log(data);
+
+  if (!data) return null;
+
   return (
-    <div>
-      <EditorForm />
-    </div>
+    <>
+      <div className='flex justify-center flex-col items-center'>
+        <EditorForm />
+
+        <div className='flex justify-center flex-col w-1/2 items-center gap-4 border-2 my-4'>
+          <h1 className='text-pretty text-2xl'>Your Documents</h1>
+          {data.map((doc: any) => (
+            <div key={doc.id}>
+              <Button variant={"link"} className='text-lg text-inherit'>
+                <Link href={`/documents/${doc.title}`} className='text-balance'>
+                  {doc.title}
+                </Link>
+              </Button>
+            </div>
+          ))}
+        </div>
+      </div>
+    </>
   );
 };
 
